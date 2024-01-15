04c1524f9cef14b1cb2bcfda75f58af5ca95bdc3
timurIsakov <timur092000@gmail.com>
2024-01-15 15:50:17 +0500

    Added get_it, and create folder for clean_architecture

---
+++ lib/application/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../features/main/presentation/screens/home_screen.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
